8320050ec9be75d4389022710c087e72200da280
Edward Birdsall <edward.birdsall@postgrad.manchester.ac.uk>
2021-08-14 17:15:53 +0100

    moved some functions to font texture manager

---
+++ sdlfontmanager.cpp
@@ -0,0 +1,79 @@
+#include "sdlfontmanager.hpp"
+
+
+void SDLFontManager::font_manager_init(
+    const SDLManager &sdlmanager,
+    std::weak_ptr<SDL_Renderer> renderer,
+    const std::string &font_path,
+    const int font_size)
+{
+
+    // TODO: these shared pointers were members of the
+    // SDLFontManager class but they do not need to be saved
+    // for future use, therefore move them to be local variables
+    // such that storage will be freed after use
+
+    std::shared_ptr<TTF_Font> m_font;
+    std::unique_ptr<SDL_Surface> m_chars_surface;
+
+
+
+        // TODO: these checks related to the initialization of the
+        // SDL libraries
+        // so this class needs to be managed from within
+        // the SDLManager class
+
+        // Font only needs to check ttf_init
+    if( sdlmanager.TTFInitSuccess() //&&
+        //m_window_init_success &&
+        //m_renderer_init_success
+        )
+    {
+        //TTF_Font *font = nullptr;
+        const int font_size = DEFAULT_FONT_SIZE; // 12
+        m_font.reset(TTF_OpenFont(font_path.c_str(), font_size),
+            TTF_CloseFont);
+
+        if(m_font.get() != nullptr)
+        {
+            m_font_init_success = true;
+
+            // NOTE: presumably these cannot fail
+            const int font_line_skip = TTF_FontLineSkip(m_font);
+            const int font_ascent = TTF_FontAscent(m_font);
+            //m_font_properties_success = true; // TODO
+
+            //if(m_font_properties_success)
+                // no point continuing if the previous step didn't work
+            {
+                // TODO: const here?
+                const std::string font_chars_string = init_font_chars_string();
+
+
+                // create the FontTextureManager
+                m_sdlfonttexturemanager.reset(
+                    new SDLFontTextureManager(
+                        font_size,
+                        font_line_skip
+                        font_ascent,
+                        font_chars_string));
+
+                // NOTE: managed internally
+                //m_sdlfonttexturemanager->set_glyph_metrics(font_chars_string);
+                //m_sdlfonttexturemanager->render_ascii_chars(font_chars_string);
+            }
+        }
+        else
+        {
+            std::cout << "Error: Could not load font from file: "
+                    << font_filename << std::endl;
+            std::cout << TTF_GetError() << std::endl;
+
+            throw TTFLibError("Error in font_init(), failed to load font from file");
+        }
+    }
+    else
+    {
+        throw TTFLibError("Error in font_init(), TTF library previously failed to initialize");
+    }
+}
---
+++ sdlfontmanager.hpp
@@ -0,0 +1,140 @@
+#ifndef SDLFONTMANAGER_HPP
+#define SDLFONTMANAGER_HPP
+
+
+#include "errortypes.hpp"
+
+
+#include "sdlfonttexturemanager.hpp"
+
+
+#include <memory>
+#include <iostream>
+
+
+
+class SDLFontManager
+{
+
+
+    public:
+
+    SDLFontManager(
+        const SDLManager &sdlmanager,
+        std::weak_ptr<SDL_Renderer> renderer,
+        const std::string &font_path,
+        const int font_size)
+        : m_font_manager_init_success(false)
+        ////, m_font_properties_success(false)
+        //, m_font_chars_init_success(false)
+        ////, m_font_render_success(false)
+        ////, m_chars_render_texture_success(false)
+        ////, m_glyphmetrics_success(false)
+        , m_font_size(font_size)
+    {
+        font_manager_init(font_path, font_size)
+    }
+
+
+    ~SDLFontManager()
+    {
+        // TODO: not actually needed, since going out of scope
+        // will delete the shared pointer
+        font_manager_destroy();
+    }
+
+
+    std::weak_ptr<SDLFontTextureManager>
+    GetSDLFontTextureManager()
+    {
+        return std::weak_ptr(m_sdlfonttexturemanager); // TODO: cast to weak?
+    }
+
+
+
+    void SDLFontManager::font_manager_init(
+        const SDLManager &sdlmanager,
+        std::weak_ptr<SDL_Renderer> renderer,
+        const std::string &font_path,
+        const int font_size);
+
+
+    void font_manager_destroy()
+    {
+        // TODO: this function should be called once the objects
+        //      std::shared_ptr<TTF_Font> m_font;
+        //      std::unique_ptr<SDL_Surface> m_chars_surface;
+        // are finished with 
+        
+
+        if(m_font_manager_init_success)
+        {
+            m_font_manager_init_success = false;
+            m_sdlfonttexturemanager.reset(); // not really needed
+        }
+    }
+
+
+    // private functions
+
+    private:
+
+
+    // create a string containing the characters to render
+    std::string init_font_chars_string()
+    {
+        //m_font_chars_string_init_success = true;
+
+        // add characters to be rendered to string
+        std::string font_chars_string;
+        for(char c = ' '; c <= '~'; ++ c)
+        {
+            font_chars_string.push_back(c);
+        }
+
+        return font_chars_string;
+    }
+
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // private data members
+    ////////////////////////////////////////////////////////////////////////////
+
+
+    private:
+
+    const int DEFAULT_FONT_SIZE = 12;
+
+    bool m_font_manager_init_success;
+    /*bool m_font_init_success;
+    bool m_chars_render_success;
+    bool m_chars_render_texture_success; // TODO: don't need to store all of these
+    bool m_glyphmetrics_success;*/
+    // the purpose of this class is to load a font from file and render it to
+    // a texture - external objects only need to know if that whole process
+    // was successful
+    // throw exceptions depending on the specific function call which failed
+    // however do not maintain the state of which function call failed
+    // internally
+
+
+    // shared pointer, this object is returned to functions
+    // which need to access the SDL_Texture and associated
+    // data (such as font size, glyph metrics)
+    std::shared_ptr<SDLFontTextureManager> m_sdlfonttexturemanager;
+
+    // map rendered font sizes to shared pointers to font texture manager
+    // objects - this permits multiple font sizes to be rendered
+    /*
+    std::map
+    <
+        float?,
+        std::shared_ptr<SDLFontTextureManager>
+    >
+        m_map_fonttexturemanager;
+    */
+    // TODO: too complex, removed
+};
+
+#endif // SDLFONTMANAGER_HPP
