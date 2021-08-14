28a5d15cf7a1f7271badab9180eb1098ebaefc05
Edward Birdsall <edward.birdsall@postgrad.manchester.ac.uk>
2021-08-14 16:54:19 +0100

    moved window creation functions to sdlresourcemanager

--- sdlmanager.hpp
+++ sdlmanager.hpp
@@ -7,6 +7,14 @@
 #include "errortypes.hpp"
 
 
+#include <memory>
+#include <iostream>
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+
 
 // singleton
 class SDLManager
@@ -27,7 +35,7 @@ class SDLManager
 
     SDLManager()
         : m_sdl_init_success{false}
-        : m_ttf_init_success{false}
+        , m_ttf_init_success{false}
         //, m_window{nullptr}
         //, m_font_size{DEFAULT_FONT_SIZE}
         //, m_window_size_x{DEFAULT_WINDOW_SIZE_X}
@@ -35,9 +43,17 @@ class SDLManager
     {
 
         // standard SDL init sequence
-        if(libs_init() == 0)
+        try
         {
-            // do nothing
+            libs_init();
+        }
+        catch(const SDLLibError &error)
+        {
+            throw;
+        }
+        catch(const TTFLibError &error)
+        {
+            throw;
         }
     }
 
@@ -53,6 +69,9 @@ class SDLManager
     void operator=(SDLManager const&) = delete;
 
 
+
+
+
     bool TTFInitSuccess() const
     {
         return m_ttf_init_success;
@@ -61,6 +80,81 @@ class SDLManager
     bool SDLInitSuccess() const
     {
         return m_sdl_init_success;
+    }
+
+
+    // functions relating to creation / destruction of
+    // SDL_Window objects
+    public:
+
+    std::weak_ptr<SDL_Window>
+    CreateWindow()
+    {
+        if(m_window.get() == nullptr)
+        {
+            std::cout << "Window Size: " << m_window_size_x << " "
+                      << m_window_size_y << std::endl;
+                      
+            const std::string window_title(
+                "SDL Text Graphics Library (C) Ed Bird 2021");
+            const unsigned short width = 800;
+            const unsigned short height = 600;
+            m_window.reset(
+                SDL_CreateWindow(
+                    window_title.c_str(),
+                    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                    width, height,
+                    SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE),
+                SDL_DestroyWindow);
+
+            if(m_window.get() == nullptr)
+            {
+                throw SDLLibError("Error: failed to create window");
+            }
+            else
+            {
+                m_renderer.reset(
+                    SDL_CreateRenderer(m_window.get(), some other stuff here?),
+                    SDL_DestroyRenderer);
+
+                if(m_renderer.get() == nullptr)
+                {
+                    throw SDLLibError("Error: failed to create renderer associated to window");
+                }
+                else
+                {
+                    // set a flag here? TODO:
+                    // no flag here, state not stored, errors managed
+                    // by emission of exception
+                }
+            }
+        }
+        else
+        {
+            throw SDLLibError("window already created");
+        }
+
+        return m_window;
+    }
+
+
+    bool WindowCreated() const
+    {
+        return (m_window.get() != nullptr);
+    }
+
+
+    void
+    DestroyWindow()
+    {
+        if(m_window.get() == nullptr)
+        {
+            throw SDLLibError("Error, no window to destroy");
+        }
+        else
+        {
+            m.window.reset();
+        }
     }
     
 
@@ -122,7 +216,16 @@ class SDLManager
     bool m_sdl_init_success;
     bool m_ttf_init_success;
 
+
+
     private:
+
+    // TODO: This class should *only* manage the SDL library resources.
+    // It should not manage the SDL windows and renderer - these are
+    // managed by the SDLResourceManager class
+    /*std::shared_ptr<SDL_Window> m_window;
+    std::shared_ptr<SDL_Renderer> m_renderer;*/
+
 
 };
 
