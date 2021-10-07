cc2103b93ee63a8ea17b9941b34bf71cc7a63754
Edward Birdsall <edward.birdsall@postgrad.manchester.ac.uk>
2021-10-07 21:23:08 +0100

    added red color

--- color.hpp
+++ color.hpp
@@ -7,11 +7,13 @@ const SDL_Color COLOR_WHITE { 255, 255, 255, 255 };
 const SDL_Color COLOR_BLACK { 0, 0, 0, 255 };
 const SDL_Color COLOR_GREEN { 0, 255, 0, 255 };
 const SDL_Color COLOR_GREY_LIGHT { 220, 220, 220, 255 };
+const SDL_Color COLOR_RED { 255, 0, 0, 255 };
 
 
 // GUI element colors
 const SDL_Color COLOR_TEXT_DEFAULT = COLOR_BLACK;
 const SDL_Color COLOR_BACKGROUND_DEFAULT = COLOR_GREY_LIGHT;
+//const SDL_Color COLOR_BACKGROUND_DEFAULT = COLOR_RED;
 
 
 #endif
