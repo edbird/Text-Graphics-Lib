8f80423b17b8873d95d01c377320ffc64f6fe4e2
Edward Birdsall <edward.birdsall@postgrad.manchester.ac.uk>
2021-10-07 20:44:16 +0100

    added non const version of helper function to prevent compiler errors, added SDL_MakeColor function

--- sdlhelper.cpp
+++ sdlhelper.cpp
@@ -1,10 +1,26 @@
 #include "sdlhelper.hpp"
 
 
-int SDL_SetRenderDrawColor(
+/*int SDL_SetRenderDrawColor(
     SDL_Renderer* renderer,
     const SDL_Color &color)
 {
     return SDL_SetRenderDrawColor(renderer,
         color.r, color.g, color.b, color.a);
+}*/
+
+// non const version
+int SDL_SetRenderDrawColor(
+    SDL_Renderer* renderer,
+    SDL_Color color)
+{
+    return SDL_SetRenderDrawColor(renderer,
+        color.r, color.g, color.b, color.a);
+}
+
+
+SDL_Color SDL_MakeColor(const int r, const int g, const int b)
+{
+    const SDL_Color color {r, g, b, 255};
+    return color;
 }
--- sdlhelper.hpp
+++ sdlhelper.hpp
@@ -5,9 +5,19 @@
 #include <SDL2/SDL.h>
 
 
+/*int SDL_SetRenderDrawColor(
+    SDL_Renderer* renderer,
+    const SDL_Color &color);*/
+
+
+// non-const version
 int SDL_SetRenderDrawColor(
     SDL_Renderer* renderer,
-    const SDL_Color &color);
+    SDL_Color color);
+
+
+SDL_Color SDL_MakeColor(const int r, const int g, const int b);
+
 
 
 #endif // SDLHELPER_HPP
